03df2274e4b61a362c5462a9aed2f124304e5bd9
shahanees1998 <shahanees1998@gmail.com>
2026-02-20 07:11:33 +0500

    fix: fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Kanit } from "next/font/google";
+import { Poppins } from "next/font/google";
 import { ClientProviders } from "./ClientProviders";
 
 import "primeflex/primeflex.css";
@@ -8,13 +8,13 @@ import "../styles/demo/Demos.scss";
 import "../styles/globals.scss";
 import "../styles/layout/layout.scss";
 import "../styles/theme.css";
-import "../styles/font-kanit.css";
+import "../styles/font-poppins.css";
 
-const kanit = Kanit({
-    weight: ["300", "400", "500"],
-    subsets: ["latin", "thai"],
+const poppins = Poppins({
+    weight: ["300", "400", "500", "600", "700"],
+    subsets: ["latin"],
     display: "swap",
-    variable: "--font-kanit",
+    variable: "--font-poppins",
 });
 
 export const metadata = {
@@ -31,18 +31,17 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <html
             lang="es"
             suppressHydrationWarning
-            className={`${kanit.variable} ${kanit.className}`}
+            className={`${poppins.variable} ${poppins.className}`}
         >
             <head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" sizes="any" />
                 <link rel="icon" href="/images/logo.png" type="image/png" sizes="32x32" />
                 <link rel="apple-touch-icon" href="/images/logo.png" />
-                {/* Fallback so "Kanit" loads if next/font variable is not set */}
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <link
-                    href="https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;500&display=swap"
+                    href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
                     rel="stylesheet"
                 />
             </head>
